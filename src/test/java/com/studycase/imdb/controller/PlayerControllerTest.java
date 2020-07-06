45f3e969f7b01cffb71163ee992eb15964ff1f6d
yavuz kocaaga <yavuz_kocaaga@yahoo.com>
2020-07-06 15:29:14 +0300

    rest api post service test added

--- src/test/java/com/studycase/imdb/controller/PlayerControllerTest.java
+++ src/test/java/com/studycase/imdb/controller/PlayerControllerTest.java
@@ -1,26 +1,31 @@
 package com.studycase.imdb.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.common.net.HttpHeaders;
+import com.studycase.imdb.controller.payload.PlayerMoviesDto;
 import com.studycase.imdb.controller.payload.request_response.PlayedTogetherRequest;
+import com.studycase.imdb.controller.payload.request_response.PlayerMoviesRequest;
 import com.studycase.imdb.controller.v1.PlayerController;
 import com.studycase.imdb.service.PlayerService;
-import net.minidev.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
-
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(PowerMockRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class PlayerControllerTest {
 
 
@@ -43,22 +48,38 @@ public class PlayerControllerTest {
                 .standaloneSetup(new PlayerController(playerService))
                 .build();
     }
+    @Test
+    public void testPlayerMovies() throws Exception {
+        PlayerMoviesRequest playerMoviesRequest = new PlayerMoviesRequest("Robert Downey Jr.");//whichever data your entity class have
+        PlayerMoviesDto playerMoviesDto = new PlayerMoviesDto();
+        Mockito.when(playerService.getPlayerMovies(playerMoviesRequest.getPlayerName())).thenReturn(playerMoviesDto);
 
-
+        mvc.perform(MockMvcRequestBuilders.post(ADDRESS_POST_ENDPOINT + "/movies")
+                .content(asJsonString(playerMoviesRequest))
+                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"));
+    }
 
     @Test
-    public void playedTogether() throws Exception {
-       /* String playerOneName = "Robert Downey Jr.";
-        String playerSecondName = "Chris Evans";
-        mvc.perform(
-                post(ADDRESS_POST_ENDPOINT + "/played-together", 1)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .param("playerOneName", playerOneName)
-                        .param("playerSecondName", playerSecondName))
-                .andDo(print())
-                .andExpect(status().isOk());
+    public void testPlayedTogether() throws Exception {
+        PlayedTogetherRequest playedTogetherRequest = new PlayedTogetherRequest("Robert Downey Jr.","Chris Evans");//whichever data your entity class have
 
-        //@formatter:on */
+        Mockito.when(playerService.playedTogether(playedTogetherRequest)).thenReturn(true);
+
+        mvc.perform(MockMvcRequestBuilders.post(ADDRESS_POST_ENDPOINT + "/played-together")
+                .content(asJsonString(playedTogetherRequest))
+                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"));
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
